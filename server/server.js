6ac025095ea8ef0e093e799dddc1dfce03880191
John Reiland <reiland.shanghai@gmail.com>
2019-12-18 23:07:59 -0500

    "Initial commit, eslint setup"

--- server/server.js
+++ server/server.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+
 const app = express();
 const port = 3003;
 const cors = require('cors');
-const db = require('../database/index.js');
 const Restaurant = require('../database/schema.js');
 
 app.use(express.static('public'));
@@ -12,19 +12,49 @@ app.use(cors());
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}...`);
-})
+});
 
 app.get('/api/restaurants/:restaurantID', (req, res) => {
-  var restId = parseInt(req.params.restaurantID);
+  const restId = parseInt(req.params.restaurantID, 10);
   // console.log("Restaurant ID: ", restId);
-  Restaurant.findOne({id: restId}).lean()
+  Restaurant.findOne({ id: restId }).lean()
     .then((doc) => {
       res.send(doc);
       // console.log(doc);
     })
     .catch((err) => {
-      console.log("Error finding restaurant in database: ", err);
+      console.log('Error finding restaurant in database: ', err);
+    });
+});
+
+app.post('/api/restaurants/', (req, res) => {
+  Restaurant.insertOne(req.data[0]).lean()
+    .then(() => {
+      res.end();
     })
-})
+    .catch((err) => {
+      console.log('Error creating restaurant in database: ', err);
+    });
+});
 
+app.put('/api/restaurants/:restaurantID', (req, res) => {
+  const restId = parseInt(req.params.restaurantID, 10);
+  Restaurant.updateOne({ id: restId }, req.data[0]).lean()
+    .then(() => {
+      res.end();
+    })
+    .catch((err) => {
+      console.log('Error updating restaurant in database: ', err);
+    });
+});
 
+app.delete('/api/restaurants/:restaurantID', (req, res) => {
+  const restId = parseInt(req.params.restaurantID, 10);
+  Restaurant.deleteOne({ id: restId }).lean()
+    .then(() => {
+      res.end();
+    })
+    .catch((err) => {
+      console.log('Error deleting restaurant from database: ', err);
+    });
+});
