08ea13cfc56dcfc573119dbb257e65debe0d9809
john <reiland.shanghai@gmail.com>
2020-01-04 01:13:12 -0500

    "Revert Data Generation"

---
+++ database/articles.js
@@ -0,0 +1,47 @@
+const faker = require('faker');
+const createCsvWriter = require('csv-writer').createObjectCsvWriter;
+
+const csvWriter = createCsvWriter({
+  path: `articles.csv`,
+  header: [
+    {id: 'id', title: 'Id'},
+    {id: 'image', title: 'Image'},
+    {id: 'title', title: 'Title'},
+    {id: 'body', title: 'Body'},
+  ],
+  fieldDelimiter: ';',
+  append: true
+});
+
+const generate = (numA, start = 0) => {
+  let articles = [];
+
+  const generateArticles = (num) => {
+    for (let i = 0; i < num; i++) {
+      const imgId = Math.floor(Math.random() * 947 + 1);
+      const objA = {
+        id: i + 1 + start,
+        image: `https://creidfecimages.s3-us-west-1.amazonaws.com/photo${imgId}.jpeg`,
+        title: faker.lorem.words(),
+        body: faker.lorem.paragraphs(),
+      };
+      articles.push(objA);
+    }
+  };
+
+  generateArticles(100000)
+
+  csvWriter
+  .writeRecords(articles)
+  .then(() => {
+    if (numA - 100000 > 0) {
+      console.log('Recursing...');
+      generate(numA - 100000, start + 100000);
+    } else {
+      console.log('The articles file was written successfully');
+    }
+  })
+  .catch(err => console.log('An error occurred during article file write: ', err))
+};
+
+generate(2000000);
--- database/faker.js
+++ database/faker.js
@@ -1,41 +1,42 @@
 const faker = require('faker');
 const Restaurant = require('./schema.js');
+const db = require('./index.js');
 
 
-const generateRestaurants = (numR, start = 0) => {
-  const restaurants = [];
+var generateRestaurants = function(numR) {
+  var restaurants = [];
 
-  const generateArticles = (numA) => {
-    const arts = [];
-    for (let i = 0; i < numA; i++) {
-      const imgId = Math.floor(Math.random() * 20 + 1);
-      const objA = {
+  var generateArticles = function(numA) {
+    var arts = [];
+    for (var i = 0; i<numA; i++) {
+      var imgId = Math.floor(Math.random() * 20 + 1);
+      var objA = {
         image: `https://creidfecimages.s3-us-west-1.amazonaws.com/photo${imgId}.jpeg`,
         title: faker.lorem.words(),
-        body: faker.lorem.paragraphs(),
-      };
+        body: faker.lorem.paragraphs()
+      }
       arts.push(objA);
     }
     return arts;
-  };
+  }
 
-  for (let j = start; j < (start + numR); j++) {
-    const numArticles = Math.floor(Math.random() * 3) + 3;
-    const objR = {
+  for (var j = 0; j<numR; j++) {
+    var numArticles = Math.floor(Math.random() * 3) + 3;
+    var objR = {
       id: (j + 1),
-      articles: null,
-    };
+      articles: null
+    }
     objR.articles = generateArticles(numArticles);
     restaurants.push(objR);
   }
 
   Restaurant.insertMany(restaurants)
-    .then(() => {
-      console.log('Restaurants successfully inserted.');
+    .then(()=> {
+      console.log('Restaurants successfully inserted.')
     })
     .catch((err) => {
       console.log(err);
-    });
-};
+    })
+}
 
-generateRestaurants(100000, 0);
+generateRestaurants(100);
